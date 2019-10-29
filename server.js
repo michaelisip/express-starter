985b3f70eda3d876d13219bb0bfff814de84df78
Michael John Isip <mj.isip123@gmail.com>
2019-10-29 18:00:29 +0800

    env

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 
+/**
+ * Load .env variables
+ */
+require('dotenv').config()
+
 // set port
-const PORT = process.env.PORT || 8081
+const PORT = process.env.SERVER_PORT || 8081
 
 /**
  * Load Middlewares
